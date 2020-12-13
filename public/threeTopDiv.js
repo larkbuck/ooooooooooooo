34db049de01aaa03318b23aba392a7b3b649f01f
Unknown <ssarratea@manas.tech>
2020-12-13 10:26:07 -0300

    Re-structure code

--- public/threeTopDiv.js
+++ public/threeTopDiv.js
@@ -4,149 +4,159 @@ import * as THREE from '/build/three.module.js';
 // const TWEEN = require('@tweenjs/tween.js') >> installed TWEEN thru NPM but not recognizing "require" - prob need to import. For now, linking to TWEEN in index.html
 
 
+// ├┬┴┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴┬┴┬┤ MOONS  ├┬┴┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴┬┴┬┤
+function GroupMoons(parent,scene){
 
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Initialization
+    let imageRadius = parent.clientWidth * .08;
+    const numberImages = 29;
+    const radianInterval = (2.0 * Math.PI) / numberImages;
+    const centerWheel = {
+        x: 0,
+        y: -900
+    }
+
+    const group = new THREE.Group();
+    group.position.set(centerWheel.x, centerWheel.y)
+    let loader = null;
+    let texture = null;
+    let material = null;
+    let circle = null;
+    let mesh = null;
+    let radius = 1500; // reset to parent.clientWidth in resize function
+
+
+
+    for (let i = 0; i < numberImages; i++) {
+        // Create a texture loader so we can load our image file
+        loader = new THREE.TextureLoader();
+        texture = loader.load('/assets-main/images/moon.jpg');
+        texture.minFilter = THREE.LinearFilter;
+
+
+        // Load an image file into a custom material
+        material = new THREE.MeshBasicMaterial({
+            map: texture,
+            transparent: true,
+            opacity: 1
+        });
+
+        circle = new THREE.CircleGeometry(imageRadius, 100);
+        mesh = new THREE.Mesh(circle, material);
+
+        mesh.material.side = THREE.DoubleSide;
+
+        mesh.position.set(
+            (Math.cos(radianInterval * i) * radius),
+            (Math.sin(radianInterval * i) * radius),
+            // (Math.sin(radianInterval * i) * radius) * .4, // Sol: I tried to make oval but then it did a CRAZY spin
+            0);
+
+        // add the image to the group
+        group.add(mesh);
+    }
+
+    scene.add(group);
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Event Listeners
+
+    // ***** snap back functionality ****
+    let wheelTheta = 0.0; // keep track of where we’ve spun the wheel
+    let spinInProgress = false; // keep track of when the wheel is spinning
+    let snapInProgress = false; // keep track of when the wheel is automatically spinning to the snapping point
+    const snapPoint = { // used to calculate properties of our snapping point
+        x: 0,
+        y: 0,
+        theta: 0.0
+    }
+
+
+    // ******* circle-spin event - just a mouse click here ******
+
+    window.addEventListener('click', () => {
+
+        // groupMoons.rotation.z += radianInterval;
+        // ^ basically just doing this but using Tween to animate:
+
+        new TWEEN.Tween(group.rotation)
+            .to({
+                z: group.rotation.z + radianInterval
+            }, 600)
+            .easing(TWEEN.Easing.Cubic.InOut)
+            .start();
+
+        for (let i = 0; i < group.children.length; i++) {
+            // group.children[i].rotation.z += radianInterval;
+            // ^ animating with Tween
+            new TWEEN.Tween(group.children[i].rotation)
+                .to({
+                    z: group.children[i].rotation.z + radianInterval
+                }, 600)
+                .easing(TWEEN.Easing.Cubic.InOut)
+                .start();
+        }
+    });
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Methods
+
+    this.update = function(){
+        TWEEN.update();
+    }
+
+
+    this.setRadius = function(width){
+        radius=width;
+        imageRadius = width * 0.08;
+    }
+
+
+}
+
+// ├┬┴┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴┬┴┬┤ INIT SCENE,CAMERA AND RENDERER  ├┬┴┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴┬┴┬┤
+const parent = document.querySelector('#threejsDiv');
 const scene = new THREE.Scene();
 // scene.background = new THREE.Color(0xFFBEE8);
-const parent = document.querySelector('#threejsDiv');
-
-
-let imageRadius = parent.clientWidth * .08;
-const numberImages = 29;
-let radius = 1500; // reset to parent.clientWidth in resize function
-const radianInterval = (2.0 * Math.PI) / numberImages;
-const centerWheel = {
-  x: 0,
-  y: -900
-}
-
-const groupMoons = new THREE.Group();
-groupMoons.position.set(centerWheel.x, centerWheel.y)
-let loader = null;
-let texture = null;
-let material = null;
-let circle = null;
-let mesh = null;
-
-
-for (let i = 0; i < numberImages; i++) {
-  // Create a texture loader so we can load our image file
-  loader = new THREE.TextureLoader();
-  texture = loader.load('/assets-main/images/moon.jpg');
-  texture.minFilter = THREE.LinearFilter;
-
-
-  // Load an image file into a custom material
-  material = new THREE.MeshBasicMaterial({
-    map: texture,
-    transparent: true,
-    opacity: 1
-  });
-
-  circle = new THREE.CircleGeometry(imageRadius, 100);
-  mesh = new THREE.Mesh(circle, material);
-
-  mesh.material.side = THREE.DoubleSide;
-
-  mesh.position.set(
-    (Math.cos(radianInterval * i) * radius),
-    (Math.sin(radianInterval * i) * radius),
-    // (Math.sin(radianInterval * i) * radius) * .4, // Sol: I tried to make oval but then it did a CRAZY spin
-    0);
-
-  // add the image to the group
-  groupMoons.add(mesh);
-}
-
-// add group to scene
-scene.add(groupMoons);
-
-
-// ├┬┴┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴┬┴┬┤ CAMERA ├┬┴┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴┬┴┬┤
 
 let camera = new THREE.PerspectiveCamera(75, 1 / .66, 0.1, 1000);
 camera.position.z = 1000;
 
-
-// ├┬┴┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴┬┴┬┤ RENDER & LISTEN ├┬┴┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴┬┴┬┤
-
 const renderer = new THREE.WebGLRenderer({
-  parent,
-  alpha: true,
-  antialias: true
+    parent,
+    alpha: true,
+    antialias: true
 });
-
 // renderer.setSize(parent.clientWidth, parent.clientHeight);
 renderer.setSize(parent.clientWidth, parent.clientWidth * .66); // set to be movie-screen ratio 1.33
-
 parent.append(renderer.domElement);
 
 
-// ***** snap back functionality ****
-let wheelTheta = 0.0; // keep track of where we’ve spun the wheel
-let spinInProgress = false; // keep track of when the wheel is spinning
-let snapInProgress = false; // keep track of when the wheel is automatically spinning to the snapping point
-const snapPoint = { // used to calculate properties of our snapping point
-  x: 0,
-  y: 0,
-  theta: 0.0
-}
+// ├┬┴┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴┬┴┬┤ INIT SCENE OBJECTS ├┬┴┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴┬┴┬┤
+const groupMoons = new GroupMoons(parent,scene);
 
-
-// ******* circle-spin event - just a mouse click here ******
-
-window.addEventListener('click', () => {
-
-  // groupMoons.rotation.z += radianInterval;
-  // ^ basically just doing this but using Tween to animate:
-
-  new TWEEN.Tween(groupMoons.rotation)
-    .to({
-      z: groupMoons.rotation.z + radianInterval
-    }, 600)
-    .easing(TWEEN.Easing.Cubic.InOut)
-    .start();
-
-  for (let i = 0; i < groupMoons.children.length; i++) {
-    // groupMoons.children[i].rotation.z += radianInterval;
-    // ^ animating with Tween
-    new TWEEN.Tween(groupMoons.children[i].rotation)
-      .to({
-        z: groupMoons.children[i].rotation.z + radianInterval
-      }, 600)
-      .easing(TWEEN.Easing.Cubic.InOut)
-      .start();
-  }
-});
-
-
-
-
-
-
+// ├┬┴┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴┬┴┬┤ LISTEN ├┬┴┬┴┬┴┤•ᴥ•ʔ├┬┴┬┴┬┴┬┤
 
 // ******** animation loop *******
 const render = function() {
-
-  TWEEN.update();
-
-  requestAnimationFrame(render);
-
-  renderer.render(scene, camera);
+    requestAnimationFrame(render);
+    groupMoons.update();
+    renderer.render(scene, camera);
 };
 
 
 // for resizing canvas when window is resized
 const resizeRenderer = function() {
   // Get width & height of parentDiv
-  let width = parent.clientWidth;
-  let height = parent.clientWidth * .66;
-  // let height = parent.clientWidth * 1.66;
+    let width = parent.clientWidth;
+    let height = parent.clientWidth * .66;
 
-  renderer.setSize(width, height);
+    console.log(`window resized: to ${width}px`);
 
-  radius = width;
-  console.log(`window resized: to ${width}px`);
-  imageRadius = width * 0.08;
+    groupMoons.setRadius(width);
+
+    renderer.setSize(width, height);
 }
 
 // Add window resize listener
