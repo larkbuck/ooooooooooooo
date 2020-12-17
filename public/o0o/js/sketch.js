8e6d48b5e8e9e348609416f4c0bcf4605488c21a
Glitch (hello-webpage) <none>
2020-12-17 13:13:18 -0800

    starting animation  + more octahedrons

---
+++ public/o0o/js/sketch.js
@@ -0,0 +1,35 @@
+// let p5Div;
+// let w = 0;
+// let h = 0;
+//
+// function setup() {
+//   p5Div = document.querySelector("#bgCanvas");
+//
+//   let c = createCanvas(windowWidth, windowHeight);
+//   // let c = createCanvas(p5Div.clientWidth, p5Div.clientWidth * .66);
+//   c.parent("#bgCanvas");
+//   // rectMode(CENTER);
+//
+// }
+//
+// function windowResized(){
+//   // resizeCanvas(p5Div.clientWidth, p5Div.clientHeight);
+//   resizeCanvas(windowWidth, windowHeight);
+// }
+//
+// function draw(){
+//   background(255);
+//
+//   rect(width/2, height/2, w, h);
+//
+//   w = width/2;
+//   h = height/2;
+//
+//   // w += 5;
+//   // h += 5;
+//   //
+//   // if (w > width){
+//   //   w = 0;
+//   //   h = 0;
+//   // }
+// }
