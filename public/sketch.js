c066113dbdf3737f2d299d0e200d67a4acd24ad4
Glitch (hello-webpage) <none>
2020-12-16 20:36:37 -0800

    stripped down, added css for placement + mailing list in menu

--- public/sketch.js
+++ public/sketch.js
@@ -1,22 +1,22 @@
-let p5Div;
-
-function setup() {
-  p5Div = document.querySelector("#p5Div");
-
-  // let c = createCanvas(p5Div.clientWidth, p5Div.clientHeight);
-  let c = createCanvas(p5Div.clientWidth, p5Div.clientWidth * .66);
-  c.parent("#p5Div");
-
-}
-
-function windowResized(){
-  // resizeCanvas(p5Div.clientWidth, p5Div.clientHeight);
-  resizeCanvas(p5Div.clientWidth, p5Div.clientWidth * .66);
-}
-
-function draw(){
-  background(13);
-  stroke(0, 255, 0);
-  noFill();
-  ellipse(width / 2, height / 2, 80, 80);
-}
+// let p5Div;
+//
+// function setup() {
+//   p5Div = document.querySelector("#p5Div");
+//
+//   // let c = createCanvas(p5Div.clientWidth, p5Div.clientHeight);
+//   let c = createCanvas(p5Div.clientWidth, p5Div.clientWidth * .66);
+//   c.parent("#p5Div");
+//
+// }
+//
+// function windowResized(){
+//   // resizeCanvas(p5Div.clientWidth, p5Div.clientHeight);
+//   resizeCanvas(p5Div.clientWidth, p5Div.clientWidth * .66);
+// }
+//
+// function draw(){
+//   background(13);
+//   stroke(0, 255, 0);
+//   noFill();
+//   ellipse(width / 2, height / 2, 80, 80);
+// }
