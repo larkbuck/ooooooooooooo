75f0fd07648c788b7bde73c748962d5bbefe758b
Unknown <ssarratea@manas.tech>
2021-01-12 19:04:26 -0300

    simple translate with css. add buttonsbefore writing intention

--- public/client.js
+++ public/client.js
@@ -64,3 +64,44 @@ let longShadowGreenEls = document.querySelectorAll(".longShadowGreen");
 for (let i = 0; i < longShadowGreenEls.length; i++) {
   longShadowGreenEls[i].style.boxShadow = shadowString;
 }
+
+
+
+/*
+
+  Simple version of translating text using css
+  https://codepen.io/JoshuaCarroll/pen/pyGYwa
+
+*/
+
+
+ function setLang(language) {
+    document.getElementById("langElement").lang = language;
+
+    // Not really needed, see below.
+      setCookie("lang", language, 365);
+      console.log("setting language",language)
+}
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+    document.getElementById("langElement").lang = getCookie("lang");
